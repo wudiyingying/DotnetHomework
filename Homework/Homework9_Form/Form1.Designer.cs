5e1e25cab8d3dbfba512b627fe63994e977410fe
颖 赵 <2044608339@qq.com>
2022-04-21 17:54:02 +0800

    9th homework

---
+++ Homework/Homework9_Form/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Homework9_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.strInfoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.strDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.strInfoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Location = new System.Drawing.Point(146, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(394, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "输入需要爬取的网页地址";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 46);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(546, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开始爬取";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.strDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.strInfoBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 381);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // strInfoBindingSource
+            // 
+            this.strInfoBindingSource.DataSource = typeof(Homework9_Form.strInfo);
+            // 
+            // strDataGridViewTextBoxColumn
+            // 
+            this.strDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.strDataGridViewTextBoxColumn.DataPropertyName = "str";
+            this.strDataGridViewTextBoxColumn.HeaderText = "爬行结果";
+            this.strDataGridViewTextBoxColumn.Name = "strDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 457);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.strInfoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn strDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource strInfoBindingSource;
+    }
+}
+
---
+++ Homework/Homework9_Form/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework9_Form
+{
+    public partial class Form1 : Form
+    {
+        private List<strInfo> strings = new List<strInfo>();
+        private Hashtable urls = new Hashtable();
+        private int count = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            strInfoBindingSource.DataSource = strings;
+            this.button1.Click += new System.EventHandler(this.btn1_Click);
+
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text != "")
+            {
+                urls.Add(textBox1.Text, false);
+            }
+            else
+            {
+                string startUrl = "http://www.cnblogs.com/dstang2000/";
+                urls.Add(startUrl, false);
+            }
+
+            new Thread(Crawl).Start();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Crawl()
+        {
+            while (true)
+            {
+                string current = null;
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url]) continue;
+                    current = url;
+                }
+
+                if (current == null || count > 10) break;
+                strings.Add(new strInfo("开始爬行"+current+"界面"));
+                string html = DownLoad(current); // 下载
+                urls[current] = true;
+
+                count++;
+                Parse(html, current);//解析,并加入新的链接
+                strings.Add(new strInfo("爬行结束"));
+                strInfoBindingSource.ResetBindings(false);
+            }
+        }
+
+        public string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+        private void Parse(string html, string current)
+        {
+            string strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+(html|aspx|jsp)[""']";
+
+            string strRef1 = @"(href|HREF)[]*=[]*[""']/[^""'#>]+(html|aspx|jsp)[""']";
+            string strRef2 = @"(href|HREF)[]*=[]*[""']./[^""'#>]+(html|aspx|jsp)[""']";
+            string strRef3 = @"(href|HREF)[]*=[]*[""']../[^""'#>]+(html|aspx|jsp)[""']";
+
+            string currentRef1 = "^(https://|http://)[^/]*";
+            string currentRef2 = "[/]$";
+            string currentRef3 = "[/][^/]*[/]$";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                if (Regex.IsMatch(match.Value, strRef1))
+                {
+                    //根目录下把相对地址转化为绝对地址
+                    string s = new Regex(currentRef1).Match(current).Value;
+
+                    strRef = s + (match.Value.Substring(match.Value.IndexOf('=') + 1)
+                          .Trim('"', '\"', '#', '>'));
+                }
+                else if (Regex.IsMatch(match.Value, strRef2))
+                {
+                    //当前目录下把相对地址转化为绝对地址
+                    string s = current.Substring(0, current.Length - 1);
+
+                    strRef = s + (match.Value.Substring(match.Value.IndexOf('=') + 1)
+                          .Trim('"', '\"', '#', '>'));
+                }
+                else if (Regex.IsMatch(match.Value, strRef3))
+                {
+                    //上一级目录下把相对地址转化为绝对地址
+                    string s = current.Substring(0, current.Length - new Regex(currentRef3).Match(current).Value.Length);
+
+                    strRef = s + (match.Value.Substring(match.Value.IndexOf('=') + 1)
+                          .Trim('"', '\"', '#', '>'));
+                }
+                else
+                {
+                    //绝对地址直接应用
+                    strRef = match.Value.Substring(match.Value.IndexOf('=') + 1)
+                              .Trim('"', '\"', '#', '>');
+                }
+
+                if (strRef.Length == 0) continue;
+                if (urls[strRef] == null) urls[strRef] = false;
+            }
+        }
+    }
+}
